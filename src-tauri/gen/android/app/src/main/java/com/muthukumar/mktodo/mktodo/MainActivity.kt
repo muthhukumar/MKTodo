5493b81dc4d0e7e7e88d98bb4adfd5497697dd46
muthukumar <muthukumarperiyasamy.mk@gmail.com>
2024-06-18 22:54:43 +0530

    changed the status bar color to match the background color

--- src-tauri/gen/android/app/src/main/java/com/muthukumar/mktodo/mktodo/MainActivity.kt
+++ src-tauri/gen/android/app/src/main/java/com/muthukumar/mktodo/mktodo/MainActivity.kt
@@ -1,3 +1,17 @@
 package com.muthukumar.mktodo.mktodo
+import android.os.Bundle
+import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 
-class MainActivity : TauriActivity()
+class MainActivity : TauriActivity(){
+   override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val window = window
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
+        window.statusBarColor = ContextCompat.getColor(this, R.color.status_bar)
+    }
+}
