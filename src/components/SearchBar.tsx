52fe9f407ceebf43b1dd71e1ddd444c5218cf387
muthukumar <muthukumarperiyasamy.mk@gmail.com>
2024-06-28 23:01:03 +0530

    ui improvements

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -11,10 +11,10 @@ export default function SearchBar() {
   }, 1200)
 
   return (
-    <form className="focus-within:ring-2 focus-within:ring-blue-500 px-1 flex items-center gap-3 border border-zinc-700 rounded-md bg-item-background">
+    <form className="focus-within:ring-2 focus-within:ring-blue-500 px-4 md:px-1 flex items-center gap-3 border border-zinc-700 rounded-md bg-item-background">
       <IoSearchOutline size={22} />
       <input
-        className="outline-none text-sm rounded-md py-1 w-full bg-item-background"
+        className="outline-none text-sm rounded-md py-3 md:py-1 w-full bg-item-background"
         placeholder="Search"
         onChange={e => {
           search(e.target.value)
