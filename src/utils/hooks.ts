728ffdc0d12582f5a63bbeb643c0fda6ad46bf7c
muthhukumar <muthukumarperiyasamy.mk@gmail.com>
2024-07-13 10:16:18 -0700

    initial audio changes

--- src/utils/hooks.ts
+++ src/utils/hooks.ts
@@ -108,3 +108,23 @@ export function usePing() {
 
   return online
 }
+
+export const useAudio = (url: string) => {
+  const [audio] = React.useState(new Audio(url))
+  const [playing, setPlaying] = React.useState(false)
+
+  const toggle = () => setPlaying(!playing)
+
+  React.useEffect(() => {
+    playing ? audio.play() : audio.pause()
+  }, [playing])
+
+  React.useEffect(() => {
+    audio.addEventListener("ended", () => setPlaying(false))
+    return () => {
+      audio.removeEventListener("ended", () => setPlaying(false))
+    }
+  }, [])
+
+  return [playing, toggle] as const
+}
