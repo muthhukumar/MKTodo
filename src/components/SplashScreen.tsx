1a27a40e9fa5db7568874068175f05063e89739d
muthhukumar <muthukumarperiyasamy.mk@gmail.com>
2024-07-15 21:08:59 +0530

    version bump

--- src/components/SplashScreen.tsx
+++ src/components/SplashScreen.tsx
@@ -2,7 +2,7 @@ import Logo from "../assets/logo.png"
 
 export default function SplashScreen() {
   return (
-    <div className="h-screen flex flex-col items-center justify-center">
+    <div className="p-3 h-screen flex flex-col items-center justify-center">
       <img src={Logo} />
       <h2 className="text-center font-bold text-6xl">MKTodo</h2>
     </div>
