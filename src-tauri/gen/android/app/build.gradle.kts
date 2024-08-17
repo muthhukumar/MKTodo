31efe6fae75e5b9d6e2c7dcc8e9b3ce9e640d4ab
muthhukumar <muthukumarperiyasamy.mk@gmail.com>
2024-08-17 22:14:22 +0530

    copy to clipboard is not working in production. But gradle upgrade is done

--- src-tauri/gen/android/app/build.gradle.kts
+++ src-tauri/gen/android/app/build.gradle.kts
@@ -12,7 +12,7 @@ plugins {
 }
 
 android {
-    compileSdk = 33
+    compileSdk = 34
     namespace = "com.muthukumar.mktodo.mktodo"
     defaultConfig {
         manifestPlaceholders["usesCleartextTraffic"] = "false"
@@ -36,7 +36,8 @@ android {
             isDebuggable = true
             isJniDebuggable = true
             isMinifyEnabled = false
-            packaging {                jniLibs.keepDebugSymbols.add("*/arm64-v8a/*.so")
+            packaging {                
+                jniLibs.keepDebugSymbols.add("*/arm64-v8a/*.so")
                 jniLibs.keepDebugSymbols.add("*/armeabi-v7a/*.so")
                 jniLibs.keepDebugSymbols.add("*/x86/*.so")
                 jniLibs.keepDebugSymbols.add("*/x86_64/*.so")
