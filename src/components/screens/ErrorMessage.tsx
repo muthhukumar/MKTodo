52faf9d68722ad48f0cb08154f5044da24fe0af5
muthhukumar <muthukumarperiyasamy.mk@gmail.com>
2024-08-31 19:42:13 +0530

    pass error boundary error to the error boundary and show the error message

--- src/components/screens/ErrorMessage.tsx
+++ src/components/screens/ErrorMessage.tsx
@@ -1,52 +1,70 @@
+import {ErrorComponentProps} from "@tanstack/react-router"
 import clsx from "clsx"
 import {useAuth} from "~/auth-context"
 import {usePing} from "~/utils/hooks"
+import ErrorBoundary from "./ErrorBoundary"
 
-export default function ErrorMessage() {
+export default function ErrorMessage(props: ErrorComponentProps) {
   const online = usePing()
   const {logout} = useAuth()
 
   return (
-    <div className="relative flex items-center justify-center flex-col p-3 h-screen w-full">
-      {online !== null && (
-        <>
-          <p
-            className={clsx("mb-5 px-4 py-1 border border-border rounded-full", {
-              "text-green-400 border-green-400": online.internet,
-              "text-red-400 border-red-400": !online.internet,
-            })}
+    <ErrorBoundary>
+      <div className="relative flex items-center justify-center flex-col p-3 h-screen w-full">
+        {online !== null && (
+          <>
+            <p
+              className={clsx("mb-5 px-4 py-1 border border-border rounded-full", {
+                "text-green-400 border-green-400": online.internet,
+                "text-red-400 border-red-400": !online.internet,
+              })}
+            >
+              Internet is {online.internet ? "Online" : "Offline"}
+            </p>
+            <p
+              className={clsx("mb-5 px-4 py-1 border border-border rounded-full", {
+                "text-green-400 border-green-400": online.server,
+                "text-red-400 border-red-400": !online.server,
+              })}
+            >
+              Server is {online.server ? "Online" : "Offline"}
+            </p>
+          </>
+        )}
+        <h2 className="font-bold text-4xl mb-4">Well, this is awkward</h2>
+        <div className="flex flex-col gap-3 text-zinc-400">
+          <p>Looks like MKTodo has crashed unexpectedly...</p>
+          <p>We've tracked the error and will get right on it.</p>
+          <details className="border border-border rounded-md p-3">
+            <summary>{props.error.name}</summary>
+            <p>
+              <strong>Message:</strong> {props.error.message}
+            </p>
+            <code>{props.error.stack}</code>
+            <code>{props.info?.componentStack}</code>
+          </details>
+        </div>
+        <div className="flex items-center gap-3">
+          <button
+            className="border border-blue-300 text-blue-300 rounded-md px-3 py-1 mt-8"
+            onClick={props.reset}
           >
-            Internet is {online.internet ? "Online" : "Offline"}
-          </p>
-          <p
-            className={clsx("mb-5 px-4 py-1 border border-border rounded-full", {
-              "text-green-400 border-green-400": online.server,
-              "text-red-400 border-red-400": !online.server,
-            })}
+            Retry
+          </button>
+          <button
+            className="border border-red-300 text-red-300 rounded-md px-3 py-1 mt-8"
+            onClick={logout}
           >
-            Server is {online.server ? "Online" : "Offline"}
-          </p>
-        </>
-      )}
-      <h2 className="font-bold text-4xl mb-4">Well, this is awkward</h2>
-      <div className="text-zinc-400">
-        <p className="mb-2">Looks like MKTodo has crashed unexpectedly...</p>
-        <p>We've tracked the error and will get right on it.</p>
+            Logout
+          </button>
+          <button
+            className="border border-zinc-600 rounded-md px-3 py-1 mt-8"
+            onClick={() => window.location.reload()}
+          >
+            Reload
+          </button>
+        </div>
       </div>
-      <div className="flex items-center gap-3">
-        <button
-          className="border border-red-300 text-red-300 rounded-md px-3 py-1 mt-8"
-          onClick={logout}
-        >
-          Logout
-        </button>
-        <button
-          className="border border-zinc-600 rounded-md px-3 py-1 mt-8"
-          onClick={() => window.location.reload()}
-        >
-          Reload
-        </button>
-      </div>
-    </div>
+    </ErrorBoundary>
   )
 }
